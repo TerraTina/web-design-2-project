919e927c356581e26fb68049870faeee9b4b6d96
Andrea Pitre <97754355+TerraTina@users.noreply.github.com>
2022-11-21 18:43:19 -0500

    Add main.js readMoreLess function and digital-media article

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+// toggle between two css styles
+function readMoreLess() {
+	// enter button's id in quotes
+	var btn = document.getElementById("");
+// enter first css class name in quotes that you want to start with
+    if (btn.className == "") {
+// enter the second css class name in quotes that you want to toggle to
+    btn.className = "";
+// enter the first css class name in quotes again
+    } else { btn.className = "";
+   }
+}
