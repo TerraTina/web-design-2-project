cc75bed5d3835dc7221adfdd9cc62037af0a68e9
Andrea Pitre <97754355+TerraTina@users.noreply.github.com>
2022-11-21 20:05:33 -0500

    Change readMoreLess function and add toggling css classes

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,14 @@
-// toggle between two css styles
-function readMoreLess() {
-	// enter button's id in quotes
-	var btn = document.getElementById("");
+// toggle between two css styles and button text
+function readMoreLess(btn) {
+  var card = btn.parentNode;
 // enter first css class name in quotes that you want to start with
-    if (btn.className == "") {
+    if (card.className == "card-closed") {
 // enter the second css class name in quotes that you want to toggle to
-    btn.className = "";
+    card.className = "card-open";
+		btn.innerHTML = "Read Less";
 // enter the first css class name in quotes again
-    } else { btn.className = "";
+    } else {
+			card.className = "card-closed";
+			btn.innerHTML = "Read More";
    }
 }
