c9823b40f1a82ac1791b0e73335bec366e4242ed
Andrea Pitre <97754355+TerraTina@users.noreply.github.com>
2022-12-14 12:14:53 -0500

    Modify JS and add CSS styling to content pages

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,33 @@
-// button click to toggle between two css styles and button text. (this) in html and (btn) in js selects for that particular button press. Works on the article button's directly nested in with .parentNode, so don't nest button further in article.
+//button click to toggle between two css styles and button text. (this) in html and (btn) in js selects for that particular button press. Works on the article button's directly nested in with .parentNode, so don't nest button further in article.
 function readMoreLess(btn) {
   var card = btn.parentNode;
-// enter first css class name in quotes that you want to start with
-    if (card.className == "card-closed") {
-// enter the second css class name in quotes that you want to toggle to
-    card.className = "card-open";
-		btn.innerHTML = "Read Less";
-// enter the first css class name in quotes again
-    } else {
-			card.className = "card-closed";
-			btn.innerHTML = "Read More";
-   }
+
+//if card is closed, open
+  var open = false;
+  if (card.className == "card-closed") {
+    open = true;
+  }
+
+//search any open-card section, close if found
+  var openCards = document.getElementsByClassName('card-open');
+  for (let q = 0; q < openCards.length; q++) {
+    var openCard = openCards[q];
+    for (var i = 0; i < openCard.childNodes.length; i++) {
+      if (openCard.childNodes[i].className == "readmoreless-button") {
+        var readMoreLessButton = openCard.childNodes[i];
+        openCard.className = "card-closed";
+        readMoreLessButton.innerHTML = "Read More";
+        break;
+      }
+    }
+  }
+
+
+
+//if card is open, change class and button text
+    if (open) {
+
+      card.className = "card-open";
+  		btn.innerHTML = "Read Less";
+    }
 }
